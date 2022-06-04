73b2a6751ce2ee9befb64430238bbbf5292ac163
dvizcarra <dvizcarra@maxentric.com>
2022-06-04 14:43:13 -0700

    updated popupModal

--- components/ExerciseModal.jsx
+++ components/ExerciseModal.jsx
@@ -1,22 +1,30 @@
 import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    Checkbox,
+    Stack,
+    Link,
+    Button,
+    Heading,
+    Text,
+    useColorModeValue,
+    useDisclosure,
     Modal,
     ModalOverlay,
     ModalContent,
-    ModalBody,
-    Box,
-    Text,
-    Button,
-    useDisclosure,
-    HStack
+    ModalBody 
   } from "@chakra-ui/react";
 
 
   export default function BeginModal(props) {
     const { isOpen, onOpen, onClose } = useDisclosure();
     const {name, description, musclegroup, equipment, type, metaData} = props.exercise;
-    //debugger;
+
     return (
-      <Box mb={[0, 2]}>
+        <Box mb={[0, 2]}>
         <Button size='lg' minW='15vw' bg='none' border='0.5px solid black'
           fontSize='xl' boxShadow='0px 0px 15px #a5a5a5' _hover={{
             boxShadow: '0px 0px 35px #a5a5a5',
@@ -34,15 +42,59 @@ import {
           />
           <ModalContent>
             <ModalBody p={6}>
-              <Text>Name: </Text>
-              <Box bg="#32CEFE">{name}</Box>
-              <Text>Description: </Text>
-              <Box bg="#32CEFE">{description}</Box>
-              <Text>MuscleGroup: </Text>
-              <Box bg="#32CEFE">{musclegroup}</Box>
+              <Flex
+                minH={'100vh'}
+                align={'center'}
+                justify={'center'}
+                bg={useColorModeValue('gray.50', 'gray.800')}>
+                <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+                    <Stack align={'center'}>
+                        <Heading fontSize={'4xl'}>Mint New Exercise</Heading>
+                        <Text fontSize={'lg'} color={'gray.600'}>
+                            Create, Share, GET SWOLE!✌️
+                        </Text>
+                    </Stack>
+                    <Box
+                        rounded={'lg'}
+                        bg={useColorModeValue('white', 'gray.700')}
+                        boxShadow={'lg'}
+                        p={8}>
+                        <Stack spacing={4}>
+                            <FormControl id="name">
+                                <FormLabel>Name:</FormLabel>
+                                <FormLabel>{name}</FormLabel>
+                            </FormControl>
+                            <FormControl id="description">
+                                <FormLabel>Description:</FormLabel>
+                                <FormLabel>{description}</FormLabel>
+                            </FormControl>
+
+                            <Stack spacing={10}>
+                                <Stack
+                                    direction={{ base: 'column', sm: 'row' }}
+                                    align={'start'}
+                                    justify={'space-between'}>
+                                    <Checkbox>Include Media</Checkbox>
+                                    <Link color={'blue.400'}>Reset</Link>
+                                </Stack>
+                                <Button
+                                    bg={'blue.400'}
+                                    color={'white'}
+                                    _hover={{
+                                    bg: 'blue.500',
+                                    }} onClick={onMint}>
+                                    Mint
+                                </Button>
+                            </Stack>
+                        </Stack>
+                    </Box>
+                </Stack>
+                </Flex>
             </ModalBody>
           </ModalContent>
         </Modal>
       </Box >
     )
   }
+
+  function onMint(){}
