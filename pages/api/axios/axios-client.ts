ae31b76f734c877dab0b510bcbcfb809af503fd0
Narbeh Shahnazarian <narbwork@gmail.com>
2022-06-04 14:30:31 -0700

    Adds pinata-serivce

--- pages/api/axios/axios-client.ts
+++ pages/api/axios/axios-client.ts
@@ -1,12 +1,5 @@
 import axios from "axios";
 
-let config = {
-    headers: {
-        pinata_api_key: process.env.PINATA_API_KEY,
-        pinata_secret_api_key: process.env.PINATA_SECRET_API_KEY
-    }
-}
-
 export const fetch = async (url: string) => {
     axios.get(url)
         .then(function (res) {
@@ -15,4 +8,20 @@ export const fetch = async (url: string) => {
         .catch(function (error) {
             console.log(`[Unable to fetch from ${url}], error=${error}`)
         })
+}
+
+export const post = async (url: string, jsonBody: string) => {
+    return axios
+    .post(url, jsonBody, {
+        headers: {
+            pinata_api_key: process.env.NEXT_PUBLIC_PINATA_API_KEY,
+            pinata_secret_api_key: process.env.NEXT_PUBLIC_PINATA_SECRET_API_KEY
+        }
+    })
+    .then(function (response) {
+        return response.data["IpfsHash"];
+    })
+    .catch(function (error) {
+        console.log(`[error communicating with service] ${error}`);
+    });
 }
