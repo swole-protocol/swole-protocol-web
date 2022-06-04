629e4359306b392728720830ba1a4006ad91ee09
PBillingsby <philbillingsby@gmail.com>
2022-06-04 15:19:30 -0700

    fix button colors

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,6 @@ export default function Home() {
         <Button size='lg' mt={12} minW='20vw' minH='5vh' bg='none' border='1.5px solid black'
           fontSize='xl' boxShadow='0px 0px 15px #a5a5a5' _hover={{
             boxShadow: '0px 0px 35px #a5a5a5'
-            // bg: '#eee', color: 'black'
           }} color='#000'>
           Get Started
         </Button>
