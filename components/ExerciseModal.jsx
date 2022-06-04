492640a308cd59d3945e0ec7b888df97c385aaa9
dvizcarra <dvizcarra@maxentric.com>
2022-06-04 16:37:41 -0700

    Added wrapping to the modals

--- components/ExerciseModal.jsx
+++ components/ExerciseModal.jsx
@@ -30,13 +30,6 @@ import {
 
     return (
         <Box mb={[0, 2]}>
-            {/* <Button size='lg' minW='15vw' bg='none' border='0.5px solid black'
-            fontSize='xl' boxShadow='0px 0px 15px #a5a5a5' _hover={{
-                boxShadow: '0px 0px 35px #a5a5a5',
-                bg: '#eee', color: 'black'
-            }} color='#32CE2E' onClick={onOpen}>
-            {name}
-            </Button> */}
             
             <Center py={6} onClick={onOpen}>
                 <Box
