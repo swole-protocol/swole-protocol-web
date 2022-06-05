d10fe5f719f3c0951df667a6a2dae77cc92238bc
PBillingsby <philbillingsby@gmail.com>
2022-06-05 03:33:43 -0700

    remove config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true,
-  assetPrefix: './',
-  images: { loader: 'custom' },
 }
 
 module.exports = nextConfig
