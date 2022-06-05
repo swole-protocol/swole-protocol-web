4c1569ba867c9d7794a69dabca646cae59d2d2e5
dvizcarra <dvizcarra@maxentric.com>
2022-06-04 17:26:23 -0700

    Updated ExerciseModal

--- components/ExerciseModal.jsx
+++ components/ExerciseModal.jsx
@@ -26,7 +26,17 @@ import {
 
   export default function BeginModal(props) {
     const { isOpen, onOpen, onClose } = useDisclosure();
-    const {name, description, musclegroup, equipment, type, metaData} = props.exercise;
+    const {
+        name,
+        description,
+        muscleGroup,
+        equipment,
+        type,
+        image,
+        video,
+        brand,
+        metadata
+    } = props.exercise;
 
     return (
         <Box mb={[0, 2]}>
@@ -40,27 +50,28 @@ import {
                     rounded={'md'}
                     overflow={'hidden'}>
                     <Stack
-                    textAlign={'center'}
-                    p={6}
-                    color={useColorModeValue('gray.800', 'white')}
-                    align={'center'}>
-                    <Text
-                        fontSize={'sm'}
-                        fontWeight={500}
-                        bg={useColorModeValue('green.50', 'green.900')}
-                        p={2}
-                        px={3}
-                        color={'green.500'}
-                        rounded={'full'}>
-                        {name}
-                    </Text>
-                    <Stack direction={'row'} align={'center'} justify={'center'}>
-                        <Text color={'gray.500'}>[Exercise Img]</Text>
-                    </Stack>
+                        textAlign={'center'}
+                        p={6}
+                        color={useColorModeValue('gray.800', 'white')}
+                        align={'center'}>
+                        <Text
+                            fontSize={'sm'}
+                            fontWeight={500}
+                            bg={useColorModeValue('green.50', 'green.900')}
+                            p={2}
+                            px={3}
+                            color={'green.500'}
+                            rounded={'full'}>
+                            {name.toUpperCase()}
+                        </Text>
+                        <Stack direction={'row'} align={'center'} justify={'center'}>
+                            <img src={image}/>
+                        </Stack>
                     </Stack>
 
                     <Box bg={useColorModeValue('gray.50', 'gray.900')} px={6} py={10}>
                         <Button
+                            onClick={addtoList}
                             w={'full'}
                             bg={'green.400'}
                             color={'white'}
@@ -97,7 +108,7 @@ import {
                 bg={useColorModeValue('gray.50', 'gray.800')}>
                 <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
                     <Stack align={'center'}>
-                        <Heading fontSize={'4xl'}>Mint New Exercise</Heading>
+                        <Heading fontSize={'4xl'}>{name.toUpperCase()}</Heading>
                         <Text fontSize={'lg'} color={'gray.600'}>
                             Create, Share, GET SWOLE!✌️
                         </Text>
@@ -108,13 +119,33 @@ import {
                         boxShadow={'lg'}
                         p={8}>
                         <Stack spacing={4}>
-                            <FormControl id="name">
+                            <FormControl>
                                 <FormLabel>Name:</FormLabel>
-                                <FormLabel>{name}</FormLabel>
+                                <FormLabel>{name.toUpperCase()}</FormLabel>
                             </FormControl>
-                            <FormControl id="description">
+                            <FormControl>
                                 <FormLabel>Description:</FormLabel>
                                 <FormLabel>{description}</FormLabel>
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel>muscleGroup:</FormLabel>
+                                <FormLabel>{muscleGroup}</FormLabel>
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel>Equipment:</FormLabel>
+                                <FormLabel>{equipment}</FormLabel>
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel>type:</FormLabel>
+                                <FormLabel>{type}</FormLabel>
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel>Video:</FormLabel>
+                                <FormLabel>{type}</FormLabel>
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel>Brand:</FormLabel>
+                                <FormLabel>{brand}</FormLabel>
                             </FormControl>
 
                             <Stack spacing={10}>
@@ -137,4 +168,4 @@ import {
     )
   }
 
-  function onMint(){}
+  function addtoList(){}
