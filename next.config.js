90517e470140190246192e356c3dd755769885d8
PBillingsby <philbillingsby@gmail.com>
2022-06-05 03:20:37 -0700

    adding out back to gitignore

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
+  assetPrefix: './',
+  images: { loader: 'custom' },
 }
 
 module.exports = nextConfig
